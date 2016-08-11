9c0a16ae9520529e70f3739ae235ed545ce333a7
Matthew King <mbs.matthew@gmail.com>
2016-08-11 01:07:34 -0700

    First attempt at Git commit

---
+++ TestGit/TestGit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string txt = "Hello Test of Git Repository";
+            Console.WriteLine(txt);
+        }
+    }
+}
